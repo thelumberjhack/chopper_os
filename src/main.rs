a90d3c443fdf04e3c5a39557343af1f4f5581f3c
Yannick Formaggio <5294889+thelumberjhack@users.noreply.github.com>
2021-02-12 18:45:47 -0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]    // don't mangle the function name
+/// Defines the new entry point.
+/// The linker looks for a function named `_start` by default.
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;    // location of the VGA Buffer as
+                                            // a raw pointer
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;      // character
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;   // color code
+        }
+    }
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
