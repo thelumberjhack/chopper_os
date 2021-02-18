9a0997c158bc898e3df84188e908ab4f6f51c7fa
Yannick Formaggio <5294889+thelumberjhack@users.noreply.github.com>
2021-02-17 19:28:35 -0800

    Tests are implemented.
    TODO: Future tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,76 @@
 #![no_std]
-// TODO: move test functions from `main.rs` here!
+#![cfg_attr(test,no_main)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+
+pub mod vga_buffer;
+pub mod serial;
+
+/// Testable trait used to automatically add print statements to test functions
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+/// Testable trait implementation for all types `T` that implements the Fn() trait
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+/// Entry point for `cargo test`
+#[cfg(test)]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+/// Exiting qemu after a test by sending the exit code to a port on the IO bus.
+/// 0xf4 is defined as the `iobase` of `isa-debug-exit` in `Cargo.toml`
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        // Writing to an IO port can generally result in arbitrary behavior
+        let mut port = Port::new(0xf4); // 0xf4 is generally unused
+        port.write(exit_code as u32);
+    }
+}
